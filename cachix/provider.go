a3ad4cd24d665e7d04eb1cb36d6df20168772557
Mika Naylor <mail@autophagy.io>
2023-03-04 12:41:11 +0100

    Init project with flake

---
+++ cachix/provider.go
@@ -0,0 +1,8 @@
+package mock
+
+import (
+	// Documentation:
+	// https://pkg.go.dev/github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema
+	//
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          terraform-provider-cachix = pkgs.buildGoModule {
+            pname = "terraform-provider-cachix";
+            inherit version;
+            src = ./.;
+
+            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.terraform-provider-cachix);
+    };
+}
